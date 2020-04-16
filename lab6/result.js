ea15006e57b50fc41e88b44612ec63f9ef9aa7a7
Aleksandr <aleksandr19_00@mail.ru>
2020-04-16 16:32:17 +0300

    Added lab6

---
+++ lab6/result.js
@@ -0,0 +1,36 @@
+
+function MfHdGXCqdF (CEWmO) {
+    
+    var elements = CEWmO.get_elements_by_tag_name("div")             
+    for(var lDjoKQA = 0; lDjoKQA < elements.length; lDjoKQA++) {
+        elements[lDjoKQA].onclick = function() {
+            alert("click on " + this.YncLiejHeUdsPDL)
+        }
+        elements[lDjoKQA].YncLiejHeUdsPDL = NkIUngSEbBdkSP
+    }
+}
+
+function DmnbbfYrw (ZLCHs, QHWuDZWtkTqLj, uMbvDUNmuaSrqFb, sXipnVXVfzSLU)  {
+    
+    uMbvDUNmuaSrqFb = uMbvDUNmuaSrqFb || true
+    sXipnVXVfzSLU = sXipnVXVfzSLU || true
+    QHWuDZWtkTqLj = QHWuDZWtkTqLj || true
+}
+
+function OQJmBKplmEXM (ImNzRC){
+    var rWcbKOgSEQ = ImNzRC.length,
+        ebBskotOmJZrPni = ImNzRC.split('').reverse().join('');
+    if (ebBskotOmJZrPni == ImNzRC) {
+      return "yes";
+    } else {
+      return "no";
+    }
+}
+
+function BaRRsGLAmob(JlbfPGpjrLcxl) {
+  if (JlbfPGpjrLcxl > 18) {
+    return true;
+  } else {
+    return confirm("Success?");
+  }
+}
